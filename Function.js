2ec33efb5c4b39c0f70d58295d80e1a6a76355f5
Debashish Debnath <bony@akij.net>
2018-07-22 12:23:14 +0600

    callback function

--- Function.js
+++ Function.js
@@ -3,3 +3,16 @@ var arr = [2,3,4,5,8,7,9]
 arr
 
 console.log(arr);
+
+function printMyInfo(data, callback){
+    if(data.age >= 28){
+        callback(data.email)
+    }
+    else{
+        callback('Email No Need')
+    }
+}
+
+printMyInfo(me, function(email){
+    console.log('Email Send to : ' + email);
+})
